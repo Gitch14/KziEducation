697f94092d02ec7c44f5ee143316b94823851932
Gitch14 <vladbuckach@ukr.net>
2022-09-15 14:52:28 +0300

    generate spring project

---
+++ src/main/java/com/example/kzieducation/KziEducationApplication.java
@@ -0,0 +1,13 @@
+package com.example.kzieducation;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class KziEducationApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(KziEducationApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/kzieducation/KziEducationApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.kzieducation;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class KziEducationApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}

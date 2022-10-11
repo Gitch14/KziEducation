d0fade161b2c222bbea5e09b0c548e0743adb301
G1tch14 <bukachvladislav@gmail.com>
2022-10-11 11:41:54 +0300

    fix(Ivanov)

--- src/main/java/com/example/kzieducation/repo/UserRepository.java
+++
@@ -1,8 +0,0 @@
-package com.example.kzieducation.repo;
-
-import com.example.kzieducation.model.User;
-
-public class UserRepository extends CrudRepository<User,Long>{
-    @Override
-    Iterable<User> findAll();
-}

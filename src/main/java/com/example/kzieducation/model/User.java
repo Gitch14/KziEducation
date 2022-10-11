257f6851d9a783b1c419cee65871688f730d25d4
G1tch14 <bukachvladislav@gmail.com>
2022-10-11 11:50:44 +0300

    fix(Ivanov)

--- src/main/java/com/example/kzieducation/model/User.java
+++ src/main/java/com/example/kzieducation/model/User.java
@@ -1,26 +1,43 @@
 package com.example.kzieducation.model;
 
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
 
-import lombok.*;
+import javax.persistence.*;
 
 @NoArgsConstructor
 @Getter
 @Setter
-@ToString
-@EqualsAndHashCode
+@Entity
+@Table(name = "user")
 public class User {
-    private Long id;
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "id", nullable = false)
+    private Integer id;
+
+    @Column(name = "surname", nullable = false, length = 200)
     private String surname;
+
+    @Column(name = "name", nullable = false, length = 200)
     private String name;
+
+    @Column(name = "`group`", nullable = false, length = 50)
     private String group;
-    private String pass;
+
+    @Column(name = "role", nullable = false, length = 15)
     private String role;
 
-    public User(String surname, String name, String group, String pass, String role) {
+    @Column(name = "pass", nullable = false, length = 20)
+    private String pass;
+
+    public User(String surname, String name, String group, String role, String pass) {
         this.surname = surname;
         this.name = name;
         this.group = group;
-        this.pass = pass;
         this.role = role;
+        this.pass = pass;
     }
+
 }
